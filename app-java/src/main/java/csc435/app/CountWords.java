4bfe8692f44b594d8fb2cbfa491f45c83d1e40e3
saamiabdulsamad123 <157726918+saamiabdulsamad123@users.noreply.github.com>
2024-01-29 21:10:49 -0600

    Update CountWords.java

--- app-java/src/main/java/csc435/app/CountWords.java
+++ app-java/src/main/java/csc435/app/CountWords.java
@@ -1,27 +1,30 @@
-package csc435.app;
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
 
-public class CountWords
-{
-    public long dataset_size = 0;
-    public double execution_time = 0.0;
+public class UniqueWordCounter {
+    public static void main(String[] args) throws IOException {
+        Path sourceDir = Paths.get(args[0]);
+        Path resultDir = Paths.get(args[1]);
 
-    public void count_words(String input_dir, String output_dir)
-    {
-        // TO-DO implement count words logic
-    }
+        Files.walk(sourceDir)
+            .filter(Files::isRegularFile)
+            .forEach(sourceFile -> {
+                try {
+                    Map<String, Integer> uniqueWordCounts = new HashMap<>();
+                    Files.readAllLines(sourceFile)
+                        .stream()
+                        .flatMap(line -> Arrays.stream(line.split("\\W+")))
+                        .forEach(word -> uniqueWordCounts.put(word, uniqueWordCounts.getOrDefault(word, 0) + 1));
 
-    public static void main(String[] args)
-    {
-        if (args.length != 2) {
-            System.err.println("improper number of arguments");
-            System.exit(1);
-        }
-
-        CountWords countWords = new CountWords();
-
-        countWords.count_words(args[0], args[1]);
-        
-        System.out.print("Finished counting " + countWords.dataset_size + " MiB of words");
-        System.out.println(" in " + countWords.execution_time + " miliseconds");
+                    Path resultFile = resultDir.resolve(sourceDir.relativize(sourceFile));
+                    Files.createDirectories(resultFile.getParent());
+                    try (PrintWriter writer = new PrintWriter(Files.newBufferedWriter(resultFile))) {
+                        uniqueWordCounts.forEach((word, count) -> writer.println(word + " " + count));
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
     }
 }
